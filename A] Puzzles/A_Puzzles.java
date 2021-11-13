dd981c467bfa8d5d7df93e2bf5c6dbcf4113a577
Abhijit Puri <85777554+Abhijitpuri17@users.noreply.github.com>
2021-11-13 14:31:29 +0530

    Update A_Puzzles.java

--- A] Puzzles/A_Puzzles.java
+++ A] Puzzles/A_Puzzles.java
@@ -51,27 +51,6 @@ public class A_Puzzles
 	      }
 	}
 	
-	static int lower_bound(int arr[], int key)
-	{
-		int pos = Arrays.binarySearch(arr, key) ;
-	    if (pos < 0)
-	    {
-	    	pos = -(pos + 1) ;
-	    }
-	    return pos ;
-		
-	}
-	
-	static int upper_bound(int arr[], int key)
-	{
-		int pos = Arrays.binarySearch(arr, key) ;
-		pos++ ;
-		if (pos < 0)
-		{
-			pos = -(pos) ;
-		}
-		return pos ;
-	}
 	
 	
 	static class FastReader {
