a2db43f180f129ab40e8c22194b05ba9c8dee3ce
Abhijit Puri <85777554+Abhijitpuri17@users.noreply.github.com>
2021-11-13 14:35:13 +0530

    Update D_TheatreSquare.java

--- D] TheatreSquare/D_TheatreSquare.java
+++ D] TheatreSquare/D_TheatreSquare.java
@@ -33,27 +33,7 @@ import java.util.*;
 		      }
 		}
 		
-		static int lower_bound(int arr[], int key)
-		{
-			int pos = Arrays.binarySearch(arr, key) ;
-		    if (pos < 0)
-		    {
-		    	pos = -(pos + 1) ;
-		    }
-		    return pos ;
-			
-		}
 		
-		static int upper_bound(int arr[], int key)
-		{
-			int pos = Arrays.binarySearch(arr, key) ;
-			pos++ ;
-			if (pos < 0)
-			{
-				pos = -(pos) ;
-			}
-			return pos ;
-		}
 		
 		
 		static class FastReader {
