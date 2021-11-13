d6f5dd2b06cbce8b5a9d53e72373efc3d87ed209
Abhijit Puri <85777554+Abhijitpuri17@users.noreply.github.com>
2021-11-13 14:30:23 +0530

    Update Y_DoubleCola.java

--- Y] DoubleCola/Y_DoubleCola.java
+++ Y] DoubleCola/Y_DoubleCola.java
@@ -62,33 +62,6 @@ package codeforces;
 		      }
 		}
 		
-		static long gcd(long a , long b)
-		{
-			if (b == 0) return a ;
-			return gcd(b, a%b) ;
-		}
-		
-		static int lower_bound(int arr[], int key)
-		{
-			int pos = Arrays.binarySearch(arr, key) ;
-		    if (pos < 0)
-		    {
-		    	pos = -(pos + 1) ;
-		    }
-		    return pos ;
-			
-		}
-		
-		static int upper_bound(int arr[], int key)
-		{
-			int pos = Arrays.binarySearch(arr, key) ;
-			pos++ ;
-			if (pos < 0)
-			{
-				pos = -(pos) ;
-			}
-			return pos ;
-		}
 		
 		
 		static class FastReader {
@@ -138,26 +111,6 @@ package codeforces;
 		    }
 		}
 		
-		static class Pair{
-			int x, y ;
-			public Pair(int x, int y)
-			{
-				this.x = x ;
-				this.y = y ;
-			}
-		}
-		
-		static void compare(Pair[] arr)
-		{
-			Arrays.sort(arr, new Comparator<Pair>() {
-				@Override
-				public int compare(Pair p1, Pair p2) {
-					if (p1.x < p2.x) return -1 ;
-					else if (p2.x < p1.x) return 1 ;
-					return 0 ;
-				}
-			});
-		}
 		
 		
 		
